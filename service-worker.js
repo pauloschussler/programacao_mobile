c93e35e4c7fdcbde073297ccdce11e357e360818
Paulo <paulojuniorpjs@hotmail.com>
2019-03-18 21:35:28 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,7 @@ self.addEventListener('fetch', event => {
           return cachedResponse
         }
 
-        return caches.open(RUNTIME).then(cache => {
+        return caches.open(PRECACHE).then(cache => {
           return fetch(event.request).then(response => {
             // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
