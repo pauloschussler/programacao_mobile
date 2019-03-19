b84146ae3dc9b8a3e05055acdf3bbf10e05b760a
Paulo <paulojuniorpjs@hotmail.com>
2019-03-18 21:24:39 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,38 @@
-const PRECACHE = 'precache-v1';
+const PRECACHE = 'precache-v1'
 
 const PRECACHE_URLS = [
-    'index.html',
-    './',
-    'demo.js',
-    'app.js'
+  'index.html',
+  './',
+  'app.js',
+  'demo.js'
 ]
 
-self.addEventListener('install', event =>{
-    event.waitUntil(
-        caches.open(PRECACHE)
-        .then(cache => cache.addAll(PRECACHE_URLS))
-        .then(self.skipWaiting())
-    )
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  )
 })
 
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response
+            })
+          })
+        })
+      })
+    )
+  }
+})
